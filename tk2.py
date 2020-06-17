afb77a1d0c90c5099456a8d96373d9a89f488ecc
yutong.guo 国与同 <180732755@Hytera.com>
2020-06-17 17:25:49 +0800

    整理布局

--- tk2.py
+++ tk2.py
@@ -1,10 +1,19 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
-
-
-from tkinter import font
 import tkinter as tk
 root = tk.Tk()
-appHighlightFont = font.Font(family='Helvetica', size=12, weight='bold')
-font.families()
+b = tk.Button(root, text = 'You')
+b.pack()
+i = 0
+def OnClick():
+    global i
+    if i == 0:
+        b.config(state = 'disabled')
+    else:
+        b.config(state = 'normal')
+    i = 1 - i
+
+
+c = tk.Button(root, text = 'Me', command = OnClick)
+c.pack()
+tk.mainloop()
